08281bcffee16b4c663bbe80e0613df4f0f3e22a
Furong(Flora) Jia <jiafurong2002@gmail.com>
2025-10-21 04:19:32 -0400

    update changes

--- main.js
+++ main.js
@@ -11,7 +11,6 @@ document.addEventListener('DOMContentLoaded', function() {
   populateResearchExperience();
   populateAcademicServices();
   populateTeaching();
-  populateTalks();
   populateHonors();
   
   // Initialize lazy loading for visitor map
